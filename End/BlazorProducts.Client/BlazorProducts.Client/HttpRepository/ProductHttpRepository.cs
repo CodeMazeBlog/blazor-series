5f3c8cb1bdaf895a6e324e73f1e3e43120db19ff
Marinko <marinkospasojevic@gmail.com>
2021-06-22 14:32:26 +0200

    Update to .NET 5

--- End/BlazorProducts.Client/BlazorProducts.Client/HttpRepository/ProductHttpRepository.cs
+++ End/BlazorProducts.Client/BlazorProducts.Client/HttpRepository/ProductHttpRepository.cs
@@ -12,22 +12,24 @@ namespace BlazorProducts.Client.HttpRepository
     public class ProductHttpRepository : IProductHttpRepository
     {
         private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _options;
 
         public ProductHttpRepository(HttpClient client)
         {
             _client = client;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         }
 
         public async Task<List<Product>> GetProducts()
         {
-            var response = await _client.GetAsync("https://localhost:5011/api/products");
+            var response = await _client.GetAsync("products");
             var content = await response.Content.ReadAsStringAsync();
             if (!response.IsSuccessStatusCode)
             {
                 throw new ApplicationException(content);
             }
 
-            var products = JsonSerializer.Deserialize<List<Product>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var products = JsonSerializer.Deserialize<List<Product>>(content, _options);
             return products;
         }
     }
