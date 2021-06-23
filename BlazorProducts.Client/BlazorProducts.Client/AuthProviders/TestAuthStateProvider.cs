6ef4d5b954734af0126d03c72dc9aec64f86cead
Marinko <marinkospasojevic@gmail.com>
2021-06-23 12:29:48 +0200

    Update to .NET 5

--- BlazorProducts.Client/BlazorProducts.Client/AuthProviders/TestAuthStateProvider.cs
+++ BlazorProducts.Client/BlazorProducts.Client/AuthProviders/TestAuthStateProvider.cs
@@ -9,13 +9,13 @@ namespace BlazorProducts.Client.AuthProviders
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(1500);
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, "John Doe"),
-                new Claim(ClaimTypes.Role, "Administrator")
-            };
-            var anonymous = new ClaimsIdentity(claims, "testAuthType");
+			await Task.Delay(1500);
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Name, "John Doe"),
+				new Claim(ClaimTypes.Role, "Administrator")
+			};
+			var anonymous = new ClaimsIdentity();
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
         }
     }
--- BlazorProducts.Client/BlazorProducts.Client/Program.cs
+++ BlazorProducts.Client/BlazorProducts.Client/Program.cs
@@ -1,32 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using BlazorProducts.Client.AuthProviders;
+using BlazorProducts.Client.HttpRepository;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Net.Http;
-using BlazorProducts.Client.HttpRepository;
-using Tewr.Blazor.FileReader;
-using Microsoft.AspNetCore.Components.Authorization;
-using BlazorProducts.Client.AuthProviders;
+using System.Threading.Tasks;
 
 namespace BlazorProducts.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
-            builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5011/api/") });
+			builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
+			builder.Services.AddAuthorizationCore();
+			builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
 
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
-
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
