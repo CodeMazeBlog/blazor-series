c5bf4162b1607eb81a858ff3c7b998a7cf5e4556
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2021-12-24 19:54:53 +0330

    Fix a small typo

--- BlazorProducts.Server/BlazorProducts.Server/Repository/RepositoryExtensions/RepositoryProductExtensions.cs
+++ BlazorProducts.Server/BlazorProducts.Server/Repository/RepositoryExtensions/RepositoryProductExtensions.cs
@@ -8,12 +8,12 @@ namespace BlazorProducts.Server.Repository.RepositoryExtensions
 {
     public static class RepositoryProductExtensions
     {
-        public static IQueryable<Product> Search(this IQueryable<Product> products, string searchTearm)
+        public static IQueryable<Product> Search(this IQueryable<Product> products, string searchTerm)
         {
-            if (string.IsNullOrWhiteSpace(searchTearm))
+            if (string.IsNullOrWhiteSpace(searchTerm))
                 return products;
 
-            var lowerCaseSearchTerm = searchTearm.Trim().ToLower();
+            var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
 
             return products.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
         }
